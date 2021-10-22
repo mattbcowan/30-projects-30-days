4faee531ee05a4a5d4200101af7b48db6cbea680
Matt C <matt@mattbcowan.com>
2021-10-21 19:58:02 -0500

    JS Half Ass Working

---
+++ 06-article-preview/scripts.js
@@ -0,0 +1,7 @@
+function showSocial() {
+  let secondary = document.getElementById("secondary-content");
+  let primary = document.getElementById("primary-content");
+
+  secondary.style.display = "block";
+  primary.style.display = "none";
+}
