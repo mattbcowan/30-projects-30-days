fbfadba24e77d84fc2f3e2f54724ee55377b7a64
Matt Cowan <matt@mattbcowan.com>
2021-11-02 17:52:51 -0500

    Day 18 - Grocery List

---
+++ 18-grocery-list/scripts.js
@@ -0,0 +1,22 @@
+let groceryItem = document.getElementById("grocery-item");
+let listOfGroceries = document.getElementById("list-of-groceries");
+let submitBtn = document.getElementById("submit-btn");
+let listItems = document.querySelectorAll(".listItem");
+
+submitBtn.onclick = () => {
+  let node = document.createElement("li");
+  node.className = "listItem";
+  node.setAttribute("id", generateId());
+  node.onclick = removeItem;
+  node.appendChild(document.createTextNode(groceryItem.value));
+  listOfGroceries.appendChild(node);
+  listItems = document.querySelectorAll(".listItem");
+};
+
+let removeItem = (e) => {
+  document.getElementById(e.srcElement.id).remove();
+};
+
+let generateId = () => {
+  return Math.floor(Math.random() * 1000000000);
+};
